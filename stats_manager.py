ce0128eff8a2591bf76c6fe5eb1c73d87234e116
Pawel Wolff <pawel.wolff@aero.obs-mip.fr>
2024-03-20 19:06:43 +0100

    (dev) stats utilites updated

--- stats_manager.py
+++ stats_manager.py
@@ -6,15 +6,23 @@ import config
 
 
 if __name__ == '__main__':
-    requests_deque_path = Path(config.APP_REQUESTS_LOG)
+    requests_deque_path = Path(config.APP_LOG_DIR) / 'requests_archive'
     if requests_deque_path.exists():
-        req = diskcache.Deque(directory=config.APP_REQUESTS_LOG)
-
-        # req = list(filter(lambda x: isinstance(x, dict), req))  # this is not needed!
-        df = pd.DataFrame.from_records(req)
-
+        req = diskcache.Deque(directory=requests_deque_path)
+        _req = []
+        for r in req:
+            _r = {'id': r['id']}
+            _r.update(r['json'])
+            _r.update(r['request'])
+            _req.append(_r)
+        df = pd.DataFrame.from_records(_req)
         print(df)
+        print()
         print(df.iloc[-1])
+        print()
         print(df['user_email'].value_counts())
+        print()
+        df2 = df[df['id'].notna()]
+        print(df2.groupby(df2.startDate.str.slice(0, 7))['id'].count())
     else:
         warnings.warn(f'{str(requests_deque_path)} does not exist')
